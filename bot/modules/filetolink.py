f52aca0e47321b11357dd9ad9216b853f06c767e
its-niloy <contact.niloybhowmick@gmail.com>
2026-05-27 00:41:44 +0600

    fix: correct get_readable_file_size import path in filetolink module

--- bot/modules/filetolink.py
+++ bot/modules/filetolink.py
@@ -6,7 +6,7 @@ from urllib.parse import quote
 from bot import LOGGER
 from bot.core.tg_client import TgClient
 from bot.core.config_manager import Config
-from bot.helper.ext_utils.bot_utils import get_readable_file_size
+from bot.helper.ext_utils.status_utils import get_readable_file_size
 from bot.helper.telegram_helper.bot_commands import BotCommands
 from bot.helper.telegram_helper.filters import CustomFilters
 from bot.helper.telegram_helper.message_utils import send_message, edit_message, delete_message
