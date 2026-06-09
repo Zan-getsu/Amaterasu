64a79a34eeb44c3d2b2c41a62bcae6ba23d835ef
its-niloy <contact.niloybhowmick@gmail.com>
2026-06-09 16:05:59 +0600

    Set Amaterasu version to v1.2.1

--- bot/version.py
+++ bot/version.py
@@ -2,14 +2,13 @@ def get_version() -> str:
     """
     Returns the version details. Do not Interfere with this !
 
-    :return: The version details in the format 'vMAJOR.MINOR.PATCH-STATE'
+    :return: The version details in the format 'vMAJOR.MINOR.PATCH'
     :rtype: str
     """
-    MAJOR = "3"
-    MINOR = "1"
-    PATCH = "3"
-    STATE = "x"
-    return f"v{MAJOR}.{MINOR}.{PATCH}-{STATE}"
+    MAJOR = "1"
+    MINOR = "2"
+    PATCH = "1"
+    return f"v{MAJOR}.{MINOR}.{PATCH}"
 
 
 if __name__ == "__main__":
