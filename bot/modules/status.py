41400e450696a314b4373af0da822cc8736dfb51
its-niloy <contact.niloybhowmick@gmail.com>
2026-06-18 02:54:53 +0600

    UI: Refine No Active Tasks message to match premium design

--- bot/modules/status.py
+++ bot/modules/status.py
@@ -43,14 +43,14 @@ async def task_status(_, message):
     if count == 0:
         currentTime = get_readable_time(time() - bot_start_time)
         free = get_readable_file_size(disk_usage(DOWNLOAD_DIR).free)
-        msg = f"""〶 <b><i>No Active Bot Tasks!</i></b>
-│
-┖ <b>NOTE</b> → <i>Each user can get status for his tasks by adding "me" or user_id like "1234xxx" after cmd: /{BotCommands.StatusCommand[0]} me or /{BotCommands.StatusCommand[1]} me</i>
+        msg = f"""<b>❖ NO ACTIVE TASKS</b>
+<code>┌─ {'CPU':<9}: {cpu_percent()}%
+├─ {'RAM':<9}: {virtual_memory().percent}%
+├─ {'Free':<9}: {free} [{round(100 - disk_usage(DOWNLOAD_DIR).percent, 1)}%]
+└─ {'Uptime':<9}: {currentTime}</code>
 
-⌬ <b><u>Bot Stats</u></b>
-┟ <b>CPU</b> → {cpu_percent()}% | <b>F</b> → {free} [{round(100 - disk_usage(DOWNLOAD_DIR).percent, 1)}%]
-┖ <b>RAM</b> → {virtual_memory().percent}% | <b>UP</b> → {currentTime}
-"""
+<b>⋗ NOTE:</b>
+<code>Each user can get status for their tasks by adding "me" or user_id like "1234xxx" after cmd: /{BotCommands.StatusCommand[0]} me or /{BotCommands.StatusCommand[1]} me</code>"""
         reply_message = await send_message(message, msg)
         await auto_delete_message(message, reply_message)
     else:
